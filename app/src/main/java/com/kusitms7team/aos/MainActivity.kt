2df1d30a8be877959a3825e5b3677cd89ea9eb8a
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2022-11-19 15:34:11 +0900

    Feat : Bottom navigaion Style

--- app/src/main/java/com/kusitms7team/aos/MainActivity.kt
+++ app/src/main/java/com/kusitms7team/aos/MainActivity.kt
@@ -2,11 +2,16 @@ package com.kusitms7team.aos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+//        main_bnv1.inflateMenu(R.menu.bottom_navigation)
         /* commit용 */
     }
 }
