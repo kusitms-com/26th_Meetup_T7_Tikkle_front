139bf6f694845ad1497edf838d80f4556daffde9
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2022-11-21 06:19:33 +0900

    Feat : Splash Animation Activity

---
+++ app/src/main/java/com/kusitms7team/aos/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.kusitms7team.aos
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.annotation.UiThread
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val backgroundImg : ImageView = findViewById(R.id.splashTextView)
+        val sideAnimation = AnimationUtils.loadAnimation(this,R.anim.anim_splash_textview)
+        backgroundImg.startAnimation(sideAnimation)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+
+    }
+}
