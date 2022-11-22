fdfa965c1ecbd4755a3b74f44f7903ca5067bc19
yuu-jeong <rladbwjd00>
2022-11-22 19:20:52 +0900

    FEAT : HomeChallenge xml 작업

--- app/src/main/java/com/kusitms7team/aos/MainActivity.kt
+++ app/src/main/java/com/kusitms7team/aos/MainActivity.kt
@@ -3,13 +3,14 @@ package com.kusitms7team.aos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var binding : ActivityMainBinding
+    //lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 //        main_bnv1.inflateMenu(R.menu.bottom_navigation)
     }
---
+++ app/src/main/java/com/kusitms7team/aos/UnabledChallengeItem.kt
@@ -0,0 +1,11 @@
+package com.kusitms7team.aos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnabledChallengeItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.item_unabled_challenge)
+    }
+}
